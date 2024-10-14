8a14b58e335efb2b1529c312d7fea4a68f9a7ccd
halil1 <halil1@DESKTOP-5H9FPR5>
2024-10-15 00:14:12 +0300

    Add project files.

---
+++ BLL/DAL/Students.cs
@@ -0,0 +1,25 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DAL
+{
+    public class Students
+    {
+        public int Id { get; set; }
+
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+
+        public decimal GPA { get; set; }
+    }
